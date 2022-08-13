da7096e3a24e9e8d7f91ba225ddaeb54c948c930
Alejandro Cremades <ale@alecrem.com>
2022-08-13 22:30:52 +0900

    Showing card names containing the entered string

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,23 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-  Text
+  Text,
+  List,
+  ListItem
 } from '@chakra-ui/react'
 import { CheckCircleIcon, NotAllowedIcon } from '@chakra-ui/icons'
 
 const Home: NextPage = (props) => {
   const [searchBox, setSearchBox] = useState('')
   const [cardIsLegal, setCardIsLegal] = useState(false)
+  const [suggestions, setSuggestions] = useState<string[]>([])
 
   const handleChange = (event: any) => {
     const { value } = event.target
     const newSearchBox = value.trim().toLowerCase()
     setSearchBox(newSearchBox)
     if (typeof newSearchBox == 'string') setCardIsLegal(isLegal(newSearchBox))
+    setSuggestions(suggestCards(newSearchBox))
   }
 
   const isLegal = (newSearchBox: string) => {
@@ -43,6 +47,33 @@ const Home: NextPage = (props) => {
     return false
   }
 
+  const findOccurrences = (
+    needleTerm: string,
+    hayObject: any,
+    limit: number = 20
+  ) => {
+    let occurrences: string[] = []
+    const keys = Object.keys(hayObject)
+    for (let i = 0; i < keys.length; i++) {
+      const key: any = keys[i]
+      let cardWeAreChecking = hayObject[key]
+      if (cardWeAreChecking === null) {
+        continue
+      }
+      if (cardWeAreChecking.toLowerCase().indexOf(needleTerm) != -1) {
+        occurrences.push(cardWeAreChecking)
+        if (occurrences.length >= limit) break
+      }
+    }
+    return occurrences
+  }
+
+  const suggestCards = (searchTerm: string, limit: number = 20) => {
+    let occurences = findOccurrences(searchTerm, legalCards.name, limit)
+    if (occurences.length > 0) return occurences
+    return findOccurrences(searchTerm, legalCards.name_ja, limit)
+  }
+
   const legalCards = (props as cardsProps).legalCards
   const placeholderIndex = ~~(
     Math.random() * Object.keys(legalCards.name).length
@@ -55,7 +86,7 @@ const Home: NextPage = (props) => {
         <Heading as="h1" size="4xl">
           Middle School Deck Check
         </Heading>
-        <Text>Enter card name in English or Japanese</Text>
+        <Text mt="1em">Enter a card name in English or Japanese</Text>
         <InputGroup mt="2em">
           <Input
             name="searchBox"
@@ -63,9 +94,23 @@ const Home: NextPage = (props) => {
             onChange={handleChange}
           />
           <InputRightElement>
-            {cardIsLegal ? <CheckCircleIcon /> : <NotAllowedIcon />}
+            {cardIsLegal ? (
+              <CheckCircleIcon color="green.500" />
+            ) : (
+              <NotAllowedIcon color="red.500" />
+            )}
           </InputRightElement>
         </InputGroup>
+        <List spacing={3} mt="1em">
+          {suggestions.map((e) => {
+            return (
+              <ListItem key={e}>
+                <CheckCircleIcon color="green.500" /> &nbsp;
+                {e}
+              </ListItem>
+            )
+          })}
+        </List>
       </Container>
     </Box>
   )
