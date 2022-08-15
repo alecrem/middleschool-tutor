22bc5110353aee9dbb78ead5f44bd2fe20dc5e11
Alejandro Cremades <ale@alecrem.com>
2022-08-15 22:56:37 +0900

    Page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { theme } from '../components/layouts/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <Head>
+        <title>Middle School Tutor</title>
+      </Head>{' '}
       <Component {...pageProps} />
     </ChakraProvider>
   )
