88f12a12c4068909297330a0f6e055be9f366d2e
Alejandro Cremades <ale@alecrem.com>
2022-08-15 22:57:15 +0900

    Undoing changes that were not needed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { theme } from '../components/layouts/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <Head>
         <title>Middle School Tutor</title>
       </Head>{' '}
