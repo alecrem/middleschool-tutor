a8ca09c9be1259c5bccfeeeaed30766e01a19a97
Alejandro Cremades Rocamora <ale@alecrem.com>
2023-06-04 11:51:37 +0900

    Fist implementation of deck list legality check

---
+++ components/DeckCheck/index.tsx
@@ -0,0 +1,91 @@
+import { FC, useState } from 'react'
+import {
+  InputGroup,
+  InputRightElement,
+  Textarea,
+  List,
+  ListItem,
+  Link
+} from '@chakra-ui/react'
+import {
+  CheckCircleIcon,
+  NotAllowedIcon,
+  ExternalLinkIcon
+} from '@chakra-ui/icons'
+import type { LegalCards } from '@/utils/dataTypes'
+import { useIsLegal } from '@/hooks/useIsLegal'
+
+interface Props {
+  legalcards: LegalCards
+}
+
+const DeckCheck: FC<Props> = (props) => {
+  const legalCards = props.legalcards
+  const { isLegal } = useIsLegal(legalCards)
+  const [mainDeck, setMainDeck] = useState<{ name: string, legal: boolean }[]>([])
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = event.target
+    const newSearchBox = value
+    // .trim().toLowerCase()
+    if (newSearchBox.length < 1) {
+      return
+    }
+    const newMainDeck = value.split("\n").filter((line) => line.trim().length > 0).map((line) => {
+      const isLegalRet = isLegal(line.toLowerCase())
+      console.log(line, isLegalRet)
+      return { name: isLegalRet.exactMatch ?? line, legal: isLegalRet.legal }
+    })
+    setMainDeck(newMainDeck)
+    console.log("newMainDeck", newMainDeck)
+    if (typeof newSearchBox == 'string') {
+    }
+  }
+
+  const deckNotLegal = () => mainDeck.filter(card => !card.legal).length > 0
+
+  const placeholderIndex = ~~(
+    Math.random() * Object.keys(legalCards.name).length
+  )
+  const placeholder = legalCards.name[placeholderIndex]
+
+  return (
+    <>
+      <InputGroup mt="2em">
+        <Textarea
+          placeholder={placeholder}
+          rows={15}
+          onChange={handleChange}
+        />
+        <InputRightElement>
+          {deckNotLegal() ? (
+            <NotAllowedIcon color="red.500" />
+          ) : (
+            <CheckCircleIcon color="green.500" />
+          )}
+        </InputRightElement>
+      </InputGroup>
+      <List spacing={3} mt="1em">
+        {mainDeck.map((card) => {
+          return (
+            <ListItem key={card.name}>
+              {card.legal ? <><CheckCircleIcon color="green.500" /> &nbsp;
+                <Link
+                  href={
+                    'https://scryfall.com/search?q=' +
+                    encodeURIComponent('prefer:oldest !"' + card + '"')
+                  }
+                  isExternal
+                >
+                  {card.name} <ExternalLinkIcon mx="2px" />
+                </Link></>
+                : <><NotAllowedIcon color="red.500" /> {card.name}</>}
+            </ListItem>
+          )
+        })}
+      </List>
+    </>
+  )
+}
+
+export { DeckCheck }
---
+++ pages/deckcheck.tsx
@@ -0,0 +1,46 @@
+import type { NextPage } from 'next'
+import { GetStaticProps } from 'next'
+import { Container, Heading, Text, Link } from '@chakra-ui/react'
+import { Header } from '@/components/Header'
+import { DeckCheck } from '@/components/DeckCheck'
+import type { CardsProps, LegalCards } from '@/utils/dataTypes'
+
+const DeckCheckPage: NextPage = (props) => {
+  const legalCards = (props as CardsProps).legalCards
+
+  return (
+    <>
+      <Header />
+      <Container maxW="container.sm" mt="2em">
+        <Heading as="h1" size="2xl">
+          Deck Check
+        </Heading>
+        <Text mt="1em">
+          Paste or type your deck list here to confirm that every card is{' '}
+          <Link href="https://www.eternalcentral.com/middleschoolrules/">
+            Middle School legal
+          </Link>.
+        </Text>
+        <DeckCheck legalcards={legalCards} />
+      </Container>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch(
+    // For offline builds, on the `middleschool-cardlist` repo directory:
+    // python3 -m http.server
+    'http://127.0.0.1:8000/output/middleschool.json'
+    // 'https://alecrem.github.io/middleschool-cardlist/output/middleschool.json'
+  )
+  const legalCards: LegalCards = await res.json()
+
+  return {
+    props: {
+      legalCards
+    }
+  }
+}
+
+export default DeckCheckPage
