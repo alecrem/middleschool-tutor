4348d6a4ef18296fe8d339ca4a09e7884d608d5a
Alejandro Cremades <ale@alecrem.com>
2023-06-29 23:34:24 +0900

    Add next-translate

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const nextTranslate = require('next-translate-plugin')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: true
 }
 
-module.exports = nextConfig
+module.exports = nextTranslate(nextConfig)
